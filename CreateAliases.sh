4c48e24811e82e6be936f305dbae9217eee3a687
Joshua Westmoreland <Joshua.Westmoreland@theice.com>
2015-09-23 11:37:48 -0400

    Added platform specific aliases

--- CreateAliases.sh
+++ CreateAliases.sh
@@ -19,9 +19,34 @@ alias p='ps -eaf'
 alias gcl='git clone'
 alias gp='git push'
 alias gco='git checkout'
+alias gcam='git commit -am'
 alias gr='git reset --hard'
 alias gc='git clean -df'
+alias g+='git add --all'
 
-#Hg
+# Hg
 alias hcl='hg clone'
 alias hf='hg fetch'
+
+# OS dependant aliases
+if [ "$(uname)" == "Darwin" ]
+then
+  alias bi='brew install'
+  alias bif='brew install --force'
+  alias bu='brew uninstall'
+  alias buf='brew uninstall --force'
+  alias br='brew remove'
+  alias brf='brew remove --force'
+  alias bc='brew cleanup'
+  alias bci='brew cask install'
+  alias bcif='brew cask install --force'
+  alias bcu='brew cask uninstall'
+  alias bcuf='brew cask uninstall --force'
+  alias bcr='brew cask remove'
+  alias bcrf='brew cask remove --force'
+  alias bcc='brew cask cleanup'
+elif [ "$(uname)" == "Linux" ]
+then
+  echo "Linux! Linux! We've got Linux here!"
+  echo "See? No one cares."
+fi
