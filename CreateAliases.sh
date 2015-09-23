9ea6ebcb4a6df6efb60a210fb3bbd780de56b229
Joshua Westmoreland <Joshua.Westmoreland@theice.com>
2015-09-23 10:48:52 -0400

    Added script to create aliases

---
+++ CreateAliases.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# author:      Josh Westmoreland
+# date:        23/09/2015
+# email:       joshua.westmoreland@me.com
+# description: Alaises for commonly used commands
+
+# General commands
+alias c='clear'
+alias d='pwd'
+alias k='kill'
+alias k9='kill -9'
+alias l='ls -alh'
+alias p='ps -eaf'
+alias del='rm -rf'
+
+# Git
+alias gcl='git clone'
+alias gp='git push'
+alias gco='git checkout'
+alias gr='git reset --hard'
+alias gc='git clean -df'
+
+#Hg
+alias hcl='hg clone'
+alias hf='hg fetch'
