fe46c108e58db7c6fe23eab570bc355d4e5c66de
Joshua Westmoreland <Joshua.Westmoreland@theice.com>
2016-01-26 13:16:38 -0500

    Updates...

--- FixDotSshDir.sh
+++ FixDotSshDir.sh
@@ -1,3 +1,4 @@
-sudo chmod -R 400 ~.ssh
+sudo chmod -R 755 ~/.ssh
+find ~/.ssh -name "*id_rsa*" -exec sudo chmod 400 {} \;
+sudo chmod 400 ~/.ssh/github_rsa*
 sudo chmod 644 ~/.ssh/known_hosts
-sudo chmod 755 ~/.ssh
